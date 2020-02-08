d22f6e142bfb1c5640b77386d52c1e93d57a69e4
Aquiles Carattino <aquiles@uetke.com>
2020-02-08 16:31:26 +0100

    Switched domain to helpdesk-klimaat.org

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["klimaathelpdesk.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["helpdesk-klimaat.org"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -124,7 +124,7 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="Klimaat Helpdesk <noreply@aquicarattino.com>"
+    "DJANGO_DEFAULT_FROM_EMAIL", default="Klimaat Helpdesk <noreply@helpdesk-klimaat.org>"
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
