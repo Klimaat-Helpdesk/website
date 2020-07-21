3400cca43da9eab8abb9417be231579b31c6cb7c
Marko Kruijer <marko@fabrique.nl>
2020-07-21 12:31:09 +0200

    fixed webpack script paths

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ module.exports = {
       },
     ]
   },
+
+  resolve: {
+    modules: [
+      '../node_modules',
+      './klimaat_helpdesk/static/stylesheets',
+      './klimaat_helpdesk/static/scripts',
+    ],
+    extensions: ['.js', '.scss'], // ,
+  },
+
   plugins: [
     new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [ 'build' ]}),  // Remove/clean build folders
     new MiniCssExtractPlugin({
