30ce0920f67cc464ce43543b2ad928af3faaa1c2
Renske Talsma <vluuks@gmail.com>
2020-08-04 14:47:44 +0200

    start with form

--- klimaat_helpdesk/core/forms.py
+++ klimaat_helpdesk/core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
+from klimaat_helpdesk.cms.models import AnswerCategory
 from klimaat_helpdesk.core.models import Question
 
 
@@ -10,3 +11,13 @@ class AskQuestion(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['user_email', 'question']
+
+
+class ClimateQuestion(forms.Form):
+    categories = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
+                              choices=AnswerCategory.objects.all())
+    main_question = forms.CharField(max_length=1000)
+    relevant_location = forms.CharField(max_length=1000)
+    relevant_timespan = forms.CharField(max_length=1000)
+    extra_info = forms.CharField(max_length=5000)
+    accept_terms = forms.BooleanField(label=_('Accept Terms & Conditions'), required=True)
