874af649d3d8241733b9fac22db080d6857ccc89
Renske Talsma <vluuks@gmail.com>
2020-08-04 14:54:11 +0200

    not all required

--- klimaat_helpdesk/core/forms.py
+++ klimaat_helpdesk/core/forms.py
@@ -15,10 +15,10 @@ class AskQuestion(forms.ModelForm):
 
 class ClimateQuestionForm(forms.Form):
     categories = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                              choices=[(c.name, c.name) for c in AnswerCategory.objects.all()])
+                              choices=[(c.name, c.name) for c in AnswerCategory.objects.all()], required=False)
     main_question = forms.CharField(max_length=1000)
-    relevant_location = forms.CharField(max_length=1000)
-    relevant_timespan = forms.CharField(max_length=1000)
-    extra_info = forms.CharField(max_length=5000)
+    relevant_location = forms.CharField(max_length=1000, required=False)
+    relevant_timespan = forms.CharField(max_length=1000, required=False)
+    extra_info = forms.CharField(max_length=5000, required=False)
     user_email = forms.EmailField()
     accept_terms = forms.BooleanField(label=_('Accept Terms & Conditions'), required=True)
