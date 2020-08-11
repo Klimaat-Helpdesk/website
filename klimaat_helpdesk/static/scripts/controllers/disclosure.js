c9b51df20edc5514bed4b8397eef7e73dd2619d6
mariekevendrik@gmail.com <mariekevendrik@gmail.com>
2020-08-11 11:12:23 +0200

    correcting aria-pattern for disclosure.

--- klimaat_helpdesk/static/scripts/controllers/disclosure.js
+++ klimaat_helpdesk/static/scripts/controllers/disclosure.js
@@ -3,30 +3,26 @@ import Controller from '../modules/controller';
 export default class DisclosureController extends Controller {
 
   init() {
-    this.link = this.element.querySelector('.disclosure__title-button')
+    this.button = this.element.querySelector('.disclosure__title-button')
     this.region = this.element.querySelector('.disclosure__disclosure-content')
 
-    if (!this.link || !this.region) {
+    if (!this.button || !this.region) {
       return
     }
 
-    this.link.addEventListener('click', () => this.toggleRegion())
+    this.button.addEventListener('click', () => this.toggleRegion())
   }
 
     toggleRegion () {
-      if (this.link.getAttribute('aria-expanded') === 'true') {
-        this.region.setAttribute('hidden', 'hidden')
-        this.link.setAttribute('aria-expanded', false)
-        this.element.classList.remove('disclosure--expanded')
+      if (this.button.getAttribute('aria-expanded') === 'true') {
+        this.region.setAttribute('aria-hidden', true)
+        this.button.setAttribute('aria-expanded', false)
       } else {
-        this.region.removeAttribute('hidden')
-        this.link.setAttribute('aria-expanded', true)
-        this.element.classList.add('disclosure--expanded')
+        this.region.setAttribute('aria-hidden', false)
+        this.button.setAttribute('aria-expanded', true)
 
         const rect = this.region.getBoundingClientRect()
         window.scroll({ top: (window.pageYOffset + rect.top) - (window.innerHeight / 3), left: 0, behavior: 'smooth' })
-
-        this.link.blur()
       }
-    }
+  }
 }
