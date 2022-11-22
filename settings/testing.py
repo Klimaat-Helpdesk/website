fd0d6697d67e707fa0df1a6ecef05c68ef2f4b63
Tom Hendrikx <tom@fourdigits.nl>
2022-11-22 15:10:25 +0100

    Upgrade to wagtail 2.13

--- settings/testing.py
+++ settings/testing.py
@@ -2,3 +2,7 @@ from .base import *  # NOQA
 
 
 SECRET_KEY = "CHANGEME!!!"
+
+DATABASES["default"]["USER"] = "klimaat-helpdesk"
+DATABASES["default"]["PASSWORD"] = "klimaat-helpdesk"
+DATABASES["default"]["HOST"] = "localhost"
