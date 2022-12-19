f8f2f0157e9e80021790e9e5a69105d2c6c70098
Tom Hendrikx <tom@fourdigits.nl>
2022-12-19 17:30:34 +0100

    Add routable page to INSTALLED_APPS

--- settings/base.py
+++ settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.contrib.settings",
+    "wagtail.contrib.routable_page",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
