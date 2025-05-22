d19530d16cf9110052cdcb099cd3927dbe4febf9
Chris Vrolijk <Chris.Vrolijk@klimaathelpdesk.org>
2025-05-22 12:47:58 +0200

    add whitenoise to development.in

--- settings/development.py
+++ settings/development.py
@@ -26,6 +26,15 @@ LOGGING = {
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 try:
     from .local import *  # NOQA
 except ImportError:
