f5948aae3c8fda0405cbfb2e792e75ec3b9e7a01
ChrisVrolijkKH <chris@vrolijk.com>
2025-11-06 08:42:34 +0100

    add various settings from base.py

--- settings/render-test.py
+++ settings/render-test.py
@@ -3,9 +3,42 @@ import dj_database_url
 from .base import *  # NOQA
 
 
-INSTALLED_APPS += [
-    # "debug_toolbar",
+INSTALLED_APPS = [
+    "apps.gitlab",
+    "apps.users",
+    "apps.frontend",
+    "wagtail_helpdesk",
+    "wagtail_helpdesk.cms",
+    "wagtail_helpdesk.core",
+    "wagtail_helpdesk.experts",
+    "wagtail_helpdesk.utils",
+    "wagtail_helpdesk.volunteers",
+    "wagtail.contrib.modeladmin",
+    "wagtail.contrib.table_block",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.redirects",
+    "wagtail.contrib.settings",
+    "wagtail.contrib.routable_page",
+    "wagtail.embeds",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.search",
+    "wagtail.admin",
+    "wagtail",
+    "modelcluster",
+    "taggit",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -30,6 +63,32 @@ DATABASES = {
     )
 }
 
+ROOT_URLCONF = "urls"
+
+MANIFEST_LOADER = {
+    "output_dir": BASE_DIR / "apps" / "frontend" / "static",
+}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "wagtail_helpdesk.utils.context_processors.defaults",
+            ],
+        },
+    },
+]
+
+AUTH_USER_MODEL = "users.User"
 ALLOWED_HOSTS = []
 DEBUG = True
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
