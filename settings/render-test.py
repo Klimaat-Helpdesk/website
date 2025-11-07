06d7cec6b8be6db4022fc7f2d942365d78486de8
ChrisVrolijkKH <chris@vrolijk.com>
2025-11-07 14:07:04 +0100

    cleanup render-test.py and move settings to render Environment vars

--- settings/render-test.py
+++ settings/render-test.py
@@ -9,43 +9,7 @@ def get_secret(secret_path, default=None):
     return Path(secret_path).read_text().strip()
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-INSTALLED_APPS = [
-    "apps.gitlab",
-    "apps.users",
-    "apps.frontend",
-    "wagtail_helpdesk",
-    "wagtail_helpdesk.cms",
-    "wagtail_helpdesk.core",
-    "wagtail_helpdesk.experts",
-    "wagtail_helpdesk.utils",
-    "wagtail_helpdesk.volunteers",
-    "wagtail.contrib.modeladmin",
-    "wagtail.contrib.table_block",
-    "wagtail.contrib.forms",
-    "wagtail.contrib.redirects",
-    "wagtail.contrib.settings",
-    "wagtail.contrib.routable_page",
-    "wagtail.embeds",
-    "wagtail.sites",
-    "wagtail.users",
-    "wagtail.snippets",
-    "wagtail.documents",
-    "wagtail.images",
-    "wagtail.search",
-    "wagtail.admin",
-    "wagtail",
-    "modelcluster",
-    "taggit",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.sitemaps",
-]
+from .base import *  # NOQA
 
 
 MIDDLEWARE = [
@@ -62,41 +26,6 @@ MIDDLEWARE = [
 
 
 # Replace the SQLite DATABASES configuration with PostgreSQL:
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        #default='postgresql://klimaathelpdeskdev_user:jRrv7yfoUkVI5Xt437CU43PS9SfXEHlo@dpg-cvr7nsvgi27c738n8log-a.frankfurt-postgres.render.com/klimaathelpdeskdev',
-        default='postgresql://klimaathelpdeskdev_user:jRrv7yfoUkVI5Xt437CU43PS9SfXEHlo@dpg-cvr7nsvgi27c738n8log-a/klimaathelpdeskdev',
-        conn_max_age=600
-    )
-}
-
-ROOT_URLCONF = "urls"
-
-MANIFEST_LOADER = {
-    "output_dir": BASE_DIR / "apps" / "frontend" / "static",
-}
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            BASE_DIR / "templates",
-        ],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "wagtail_helpdesk.utils.context_processors.defaults",
-            ],
-        },
-    },
-]
-
-AUTH_USER_MODEL = "users.User"
 ALLOWED_HOSTS = []
 DEBUG = True
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -104,28 +33,6 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 ALLOWED_HOSTS.append("test.klimaathelpdesk.org")
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "CHANGEME!!!"
-
-BASE_URL = WAGTAILADMIN_BASE_URL = "http://localhost:8000"
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
-
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-}
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
@@ -140,7 +47,5 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-WAGTAIL_SITE_NAME = "klimaat-helpdesk"
-WAGTAILADMIN_BASE_URL = BASE_URL = "https://klimaathelpdesk.org"
 
 
